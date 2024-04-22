eb3cdaab1aaa2994a3b61d0bd7b988594f983408
Wytamma Wirth <wytamma.wirth@me.com>
2024-04-22 17:59:09 +1000

    :sparkles: create_workflow_v8

--- src/snk_cli/conda.py
+++ src/snk_cli/conda.py
@@ -8,20 +8,37 @@ import os
 
 from snk_cli.utils import check_command_available
 from snakemake.deployment.conda import Env
+from snakemake.persistence import Persistence
 import snakemake
 
 snakemake_version = version.parse(snakemake.__version__)
+is_snakemake_version_8_or_above = snakemake_version >= version.parse('8')
+
+@dataclass
+class PersistenceMock(Persistence):
+    """
+    Mock for workflow.persistence
+    """
+    conda_env_path: Path = None
+    _metadata_path: Path = None
+    _incomplete_path: Path = None
+    shadow_path: Path = None
+    conda_env_archive_path: Path = None
+    container_img_path: Path = None
+    aux_path: Path = None
 
 
-def create_workflow_v7(conda_prefix):
-    
+def get_frontend():
     if check_command_available("mamba"):
         conda_frontend = "mamba"
     else:
         conda_frontend = "conda"
-    
+    return conda_frontend
+
+def create_workflow_v7(conda_prefix):
     from snakemake.workflow import Workflow
     
+    conda_frontend = get_frontend()
     workflow = Workflow(
         snakefile=Path(),
         overwrite_config=dict(),
@@ -31,23 +48,6 @@ def create_workflow_v7(conda_prefix):
         conda_frontend=conda_frontend,
         use_conda=True,
     )
-    
-    from snakemake.persistence import Persistence
-    
-    @dataclass
-    class PersistenceMock(Persistence):
-        """
-        Mock for workflow.persistence
-        """
-
-        conda_env_path: Path = None
-        _metadata_path: Path = None
-        _incomplete_path: Path = None
-        shadow_path: Path = None
-        conda_env_archive_path: Path = None
-        container_img_path: Path = None
-        aux_path: Path = None
-
 
     persistence = PersistenceMock(
         conda_env_path=Path(conda_prefix).resolve() if conda_prefix else None,
@@ -60,21 +60,42 @@ def create_workflow_v7(conda_prefix):
     return workflow
 
 def create_workflow_v8(
-        snakefile,
-        config,
-        configfiles,
-        conda_prefix,
-        workdir=None,
-        config_args=None,
-        use_conda=True,
+        conda_prefix
     ):
-    raise NotImplementedError("This function is not implemented yet")
+    from snakemake.api import Workflow
+    from snakemake.settings import (
+        ConfigSettings,
+        DeploymentSettings,
+        ResourceSettings,
+        WorkflowSettings,
+        StorageSettings,
+    )
+    conda_frontend = get_frontend()
+    workflow = Workflow(
+        config_settings=ConfigSettings(),
+        resource_settings=ResourceSettings(),
+        workflow_settings=WorkflowSettings(),
+        storage_settings=StorageSettings(),
+        deployment_settings=DeploymentSettings(
+            conda_frontend=conda_frontend, 
+            conda_prefix=conda_prefix
+        ),
+    )
+    persistence = PersistenceMock(
+        conda_env_path=Path(conda_prefix).resolve() if conda_prefix else None,
+        conda_env_archive_path=os.path.join(Path(".snakemake"), "conda-archive"),
+    )
+    if hasattr(workflow, "_persistence"):
+        workflow._persistence = persistence
+    else:
+        workflow.persistence = persistence
+    return workflow
 
 def conda_environment_factory(env_file_path: Path, conda_prefix_dir_path: Path) -> Env:
     """
     Create a snakemake environment object from a given environment file and conda prefix directory
     """
-    if snakemake_version >= version.parse('8'):
+    if is_snakemake_version_8_or_above:
         snakemake_workflow = create_workflow_v8(
             conda_prefix_dir_path
         )
