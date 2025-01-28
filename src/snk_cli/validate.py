a97289431a6b3903637a1b63cc57339d50382dd2
Wytamma Wirth <wytamma.wirth@me.com>
2025-01-28 22:06:09 +1100

    :bug: update validate to work with Dict type

--- src/snk_cli/validate.py
+++ src/snk_cli/validate.py
@@ -57,25 +57,33 @@ def validate_and_transform_in_place(config: Dict[str, Any], validation: Validati
                 raise ValueError(f"Unknown type '{val_info['type']}'")
             try:
                 if getattr(val_type, "__origin__", None) is list:
-                    # List
+                    # List e.g. List[str]
                     val_type = val_type.__args__[0]
                     if not isinstance(value, list):
                         raise ValueError(f"Expected a list for key '{key}'")
                     config[key] = [val_type(v) for v in value]
                 elif get_origin(val_type) is tuple:
-                    # Tuple
-                    assert len(value) == 2, f"Expected a list of length 2 for key '{key}'"
+                    # Tuple e.g. Tuple[str, int]
+                    assert len(value) == 2, f"Tuple type should have 2 elements for key '{key}'"
                     key_type = val_type.__args__[0]
                     val_type = val_type.__args__[1]
                     config[key] = [key_type(value[0]), val_type(value[1])]
                 elif get_origin(val_type) is dict:
-                    # Dict
-                    config[key] = val_type(value)
+                    # Dict e.g. Dict[str, int]
+                    if val_type.__args__ is None:
+                        config[key] = value
+                        continue
+                    assert len(val_type.__args__) == 2, f"Dict type should have 2 arguments for key '{key}'"
+                    dictionary = {}
+                    key_type = val_type.__args__[0]
+                    val_type = val_type.__args__[1]
+                    for k, v in value.items():
+                        dictionary[key_type(k)] = val_type(v)
                 else:
                     # basic
                     config[key] = val_type(value)
             except (ValueError, TypeError) as e:
-                raise ValueError(f"Type conversion error for key '{key}': {e}")
+                raise ValueError(f"Type conversion error for key '{key}'") from e
         elif isinstance(value, dict):
             # Nested dictionary validation
             validate_and_transform_in_place(value, val_info)
